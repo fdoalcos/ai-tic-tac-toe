fd5f71366d999b240641ab1a9c85e8cbdc9f36bd
Francis Alcos <alcosfrancis@gmail.com>
2026-02-23 16:03:22 -0500

    added docker + github actions

--- app.py
+++ app.py
@@ -388,4 +388,6 @@ def api_ai_move():
 
 if __name__ == "__main__":
     # Go to http://127.0.0.1:5000
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
